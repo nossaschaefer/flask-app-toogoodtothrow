93306f88e8cae76f8a3969754c198422aacd8b68
Nossa Schaefer <nossa.schaefer@gmail.com>
2024-12-08 15:12:12 +0100

    Save changes before rewriting history

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -14,7 +14,7 @@ API_KEY = os.getenv('API_KEY')
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
